6c8761652802c113c3829a56470276a492ba29c5
Peicles S. da C. Jr <pericles.scj@gmail.com>
2022-01-24 18:40:38 -0300

    Add compute variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,32 @@ variable "auth_mode" {
 variable "oci_profile" {
   description = "OCI profile"
 }
+
+variable "oci_ad" {
+  description = "OCI availability domain"
+}
+
+variable "oci_compartment_id" {
+  description = "OCI compartment id"
+}
+
+variable "oci_inst_name" {
+  description = "OCI instance name"
+}
+
+variable "oci_inst_shape" {
+  description = "OCI instance shape"
+}
+
+variable "oci_inst_src_type" {
+  description = "OCI instance type"
+}
+
+variable "oci_inst_src_id" {
+  description = "OCI instance source id"
+}
+
+variable "oci_ssh_keys" {
+  description = "OCI ssh key file path"
+}
+
