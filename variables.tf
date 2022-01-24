28f7a1dbeefc045850be2540d9fadba4ab2c5d05
Peicles S. da C. Jr <pericles.scj@gmail.com>
2022-01-24 18:11:58 -0300

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "oci_region" {
+  description = "OCI region name"
+}
+
+variable "auth_mode" {
+  description = "OCI auth mode"
+}
+
+variable "oci_profile" {
+  description = "OCI profile"
+}
