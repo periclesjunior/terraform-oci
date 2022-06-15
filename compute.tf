91508a31bdf30c870cc745bcd004accac0ec83db
Pericles S. da C. Jr <pericles.scj@gmail.com>
2022-06-15 17:49:54 -0300

    Fix compute.tf

--- compute.tf
+++ compute.tf
@@ -2,12 +2,12 @@ resource "oci_core_instance" "vm" {
   availability_domain = var.oci_ad
   compartment_id = var.oci_compartment_id
   display_name = var.oci_inst_name
-  preserve_boot_volume = false
   shape = var.oci_inst_shape
+  preserve_boot_volume = false
 
   source_details {
     source_type = var.oci_inst_src_type
-    source_id = oci_inst_src_id
+    source_id = var.oci_inst_src_id
   }
 
   create_vnic_details {
